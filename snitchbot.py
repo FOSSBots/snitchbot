63524e7383057d289e3ca7996e6f346e6aac8d0e
MacFan4000 <paulfkeffer@gmail.com>
2020-07-08 10:41:14 -0400

    custom changes

--- snitchbot.py
+++ snitchbot.py
@@ -5,6 +5,7 @@ import collections
 import re
 import sqlite3
 import sre_constants
+import os
 
 from twisted.internet import protocol, reactor, task
 from twisted.python import log
@@ -14,10 +15,10 @@ import settings
 
 '''
 CREATE TABLE rules (
-    wiki text, 
-    type text, 
-    pattern text, 
-    channel text, 
+    wiki text,
+    type text,
+    pattern text,
+    channel text,
     ignore integer,
     UNIQUE(wiki, type, pattern, channel, ignore)
 );
@@ -36,9 +37,10 @@ def strip_formatting(message):
     """Strips colors and formatting from IRC messages"""
     return COLOR_RE.sub('', message)
 
-ACTION_RE = re.compile(r'\[\[(.+)\]\] (?P<log>.+)  \* (?P<user>.+) \*  (?P<summary>.+)')
+ACTION_RE = re.compile(r'(?P<wiki>.+) \[\[(.+)\]\] (?P<log>.+)  \* (?P<user>.+) \*  (?P<summary>.+)')
 
 DIFF_RE = re.compile(r'''
+    (?P<wiki>.*)
     \[\[(?P<page>.*)\]\]\        # page title
     (?P<patrolled>!|)            # patrolled
     (?P<new>N|)                  # new page
@@ -52,10 +54,10 @@ DIFF_RE = re.compile(r'''
 
 
 class EternalClient(irc.IRCClient):
-    
+
     def __init__(self):
         self.pingger = task.LoopingCall(self.pingServer)
-        
+
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
         self.pingger.start(60, now=False)
@@ -63,19 +65,20 @@ class EternalClient(irc.IRCClient):
     def connectionLost(self, reason):
         irc.IRCClient.connectionLost(self, reason)
         self.pingger.stop()
-        
+
     def pingServer(self):
         self.sendLine('PING %s' % self.transport.connector.host)
-        log.msg('%s sent ping to %s' 
+        log.msg('%s sent ping to %s'
             % (self.nickname, self.transport.connector.host))
-        
+
     def irc_PONG(self, prefix, params):
-        log.msg('%s received ping from %s' 
+        log.msg('%s received ping from %s'
             % (self.nickname, params[1]))
 
 class Snatch(EternalClient):
-    realname = 'snerk'
-    nickname = '<Replace with Bot Nick>'
+    realname = 'Recent Changes'
+    nickname = 'StreamBot-read'
+    password = ':ZppixBot %s' % (settings.nickserv_password)
 
     def connectionMade(self):
         EternalClient.connectionMade(self)
@@ -84,6 +87,7 @@ class Snatch(EternalClient):
         self.factory.snatches.append(self)
         self.channels = set()
         self.cursor = self.factory.connection.cursor()
+        self.join('#miraheze-feed')
 
     def connectionLost(self, reason):
         EternalClient.connectionLost(self, reason)
@@ -103,13 +107,15 @@ class Snatch(EternalClient):
         self.channels.discard(channel)
 
     def privmsg(self, user, channel, message):
-        wiki = channel[1:]
+        content = message.split(' ')
+        wiki = content[0]
         cleaned_message = strip_formatting(message)
         edit_match = DIFF_RE.match(cleaned_message)
         action_match = ACTION_RE.match(cleaned_message)
         match = edit_match or action_match
         if not match:
             log.msg('%s was not matched' % repr(cleaned_message))
+            log.msg('Wiki: %s Page: %s User: %s' % wiki)
             return
         diff = match.groupdict()
         self.cursor.execute(
@@ -150,17 +156,17 @@ class Snatch(EternalClient):
                 ignore.append(rule.channel)
             else:
                 for snitch in self.factory.snitches:
-                    snitch.tattle(rule, diff)
+                    snitch.tattle(rule, diff, wiki)
                     ignore.append(rule.channel)
 
     def syncChannels(self):
         log.msg('Syncing snatch\'s channels')
         self.cursor.execute(
             'SELECT wiki FROM rules')
-        channels = set('#%s' % row[0] for row in self.cursor.fetchall())
+        channels = set('#miraheze-feed')
         [self.join(channel) for channel in (channels - self.channels)]
         [self.part(channel) for channel in (self.channels - channels)]
-        
+
     def quit(self):
         irc.IRCClient.quit(self)
         self.factory.stopTrying()
@@ -168,9 +174,9 @@ class Snatch(EternalClient):
 
 
 class Snitch(EternalClient):
-    realname = 'snerk'
-    nickname = '<Replace with Bot Nick>'
-    password = ':%s %s' % (nickname, settings.nickserv_password)
+    realname = 'Recent Changes'
+    nickname = '%s' % (settings.nickname)
+    password = ':ZppixBot %s' % (settings.nickserv_password)
     lineRate = 1
 
     def connectionMade(self):
@@ -193,7 +199,7 @@ class Snitch(EternalClient):
         for row in self.cursor.fetchall():
             self.channels.add(row[0])
             self.join(row[0])
-            
+
     def joined(self, channel):
         log.msg('Snitch joined: %s' % channel)
         self.channels.add(channel)
@@ -205,7 +211,7 @@ class Snitch(EternalClient):
     def updateRules(self, channel, params, ignore=False, remove=False):
         if len(params) < 2:
             self.msg(channel,
-                '!(un)stalk wiki (page|user|summary|log|all) [pattern]')
+                '!(un)watch wiki (page|user|summary|log|all) [pattern]')
             return
         wiki = params[0]
         rule_type = params[1]
@@ -228,15 +234,15 @@ class Snitch(EternalClient):
                 return
         self.cursor.execute(
             'SELECT * FROM rules WHERE \
-            wiki=? AND type=? AND pattern=? AND channel=? AND ignore=?', 
+            wiki=? AND type=? AND pattern=? AND channel=? AND ignore=?',
             (wiki, rule_type, pattern, channel, ignore))
         exists = self.cursor.fetchone()
-        
+
         if remove:
             if exists:
                 self.cursor.execute(
                     'DELETE FROM rules WHERE \
-                    wiki=? AND type=? AND pattern=? AND channel=? AND ignore=?', 
+                    wiki=? AND type=? AND pattern=? AND channel=? AND ignore=?',
                     (wiki, rule_type, pattern, channel, ignore))
                 self.msg(channel, 'Rule deleted.')
             else:
@@ -246,11 +252,9 @@ class Snitch(EternalClient):
                 self.msg(channel, 'Rule already exists.')
             else:
                 self.cursor.execute(
-                    'INSERT OR REPLACE INTO rules VALUES (?,?,?,?,?)', 
+                    'INSERT OR REPLACE INTO rules VALUES (?,?,?,?,?)',
                     (wiki, rule_type, pattern, channel, ignore))
                 self.msg(channel, 'Rule added.')
-        for snatch in self.factory.snatches:
-            snatch.syncChannels()
 
     def privmsg(self, sender, channel, message):
         if not sender:
@@ -261,20 +265,22 @@ class Snitch(EternalClient):
             message = message[1:]
         else:
             return
-        
+
         user = sender.split('!', 1)[0]
         hostmask = sender.split('@', 1)[1]
         action = message.split(' ')[0]
         params = message.split(' ')[1:]
 
-        if action == 'stalk':
+        if action == 'watch':
             self.updateRules(channel, params)
         elif action == 'ignore':
             self.updateRules(channel, params, ignore=True)
-        elif action == 'unstalk':
+        elif action == 'unwatch':
             self.updateRules(channel, params, remove=True)
         elif action == 'unignore':
             self.updateRules(channel, params, ignore=True, remove=True)
+        elif action == 'info':
+            self.msg(channel, 'I am running StreamBot version Alpha. To see my commands say !help')
         elif action == 'list':
             self.cursor.execute(
                 'SELECT * FROM rules WHERE channel=?', (channel,))
@@ -285,61 +291,96 @@ class Snitch(EternalClient):
             if re.search(r'(monowatchlist|monowiki)', params[0], re.I):
                 self.msg(channel, "FUCK OFF.")
             elif not params:
-                self.msg(channel, '!join (channel)')
+                if hostmask in settings.authorized_users:
+                    self.msg(channel, '!join (channel)')
+                else:
+                    self.msg(channel, 'Permission denied. Login as an administrator to use this command.')
             else:
-                self.cursor.execute(
-                    'INSERT OR IGNORE INTO channels VALUES (?)', (params[0],))
-                self.join(params[0])
+                if hostmask in settings.authorized_users:
+                    self.msg(channel, 'Now joining channel')
+                    self.cursor.execute(
+                        'INSERT OR IGNORE INTO channels VALUES (?)', (params[0],))
+                    self.join(params[0])
+                else:
+                    self.msg(channel, 'Permission denied. Login as an administrator to use this command.')
         elif action == 'part':
+            if hostmask in settings.authorized_users:
+                self.msg(channel, 'Now parting channel')
+                self.cursor.execute(
+                    'DELETE FROM channels WHERE name=?', (params[0],))
+                self.part(params[0])
+            else:
+                self.msg(channel, 'Permission denied. Login as an administrator to use this command.')
+        elif action == 'getout':
+            self.msg(channel, 'Okay okay!')
             self.cursor.execute(
                 'DELETE FROM channels WHERE name=?', (channel,))
             self.part(channel)
         elif action == 'help':
-            self.msg(channel, 
-                '!(stalk|ignore|unstalk|unignore|list|join|part|quit)')
+            self.msg(channel,
+                '!(watch|ignore|unwatch|unignore|list|join|part|quit)')
+        elif action == 'restart':
+            if hostmask in settings.authorized_users:
+                self.msg(channel, 'Now restarting...')
+                log.msg('Got restart command. Running suicide script')
+                os.system('/data/project/xslack/mh-stream-beta/internal/suicide')
+            else:
+                self.msg(channel, 'Permission denied. Login as an administrator to use this command.')
         elif action == 'quit':
             if hostmask in settings.authorized_users:
                 log.msg('Quitting')
+                self.msg(channel, 'Goodbye!')
                 self.quit()
                 for snatch in self.factory.snatches:
                     snatch.quit()
+            else:
+                self.msg(channel, 'Permission denied. Login as an administrator to use this command.')
         else:
             if hostmask in settings.authorized_users:
                 self.sendLine(message)
-                    
+
         def quit(self):
             irc.IRCClient.quit(self)
             self.factory.stopTrying()
             self.transport.loseConnection()
 
-    def tattle(self, rule, diff):
+    def tattle(self, rule, diff, wiki):
         if rule.channel not in self.channels:
             return
         if 'page' in diff:
             if not diff['summary']:
                 diff['summary'] = '[none]'
-            self.msg(rule.channel, '; '.join (('[[%s]]' 
-                % diff['page'], diff['user'], diff['summary'], diff['url'].replace('http://', 'https://'))))
+            self.msg(rule.channel, '\2[[%s]]\2 on %s was edited by \2%s\2 with the following comment: %s Link: %s'
+                % (diff['page'], wiki, diff['user'], diff['summary'], diff['url'].replace('http://', 'https://')))
         else:
-            self.msg(rule.channel, '%s %s; https://%s.org/wiki/Special:Log/%s' 
-                % (diff['user'], diff['summary'], rule.wiki.strip('.org'), diff['log']))
+            if diff['log'] == 'requestwiki':
+                url = 'https://meta.miraheze.org/wiki/Special:RequestWikiQueue'
+            elif diff['log'] == 'createwiki':
+                url = 'https://meta.miraheze.org/wiki/Special:RequestWikiQueue'
+            elif diff['log'] == 'unblock':
+                url = 'https://' + wiki + '.miraheze.org/wiki/Special:Log/block'
+            else:
+                url = wiki.strip('wiki')
+                url = 'https://' + url + '.miraheze.org/wiki/Special:Log/' + diff['log']
+            self.msg(rule.channel, 'On %s \2%s\2 %s; %s'
+                % (wiki, diff['user'], diff['summary'], url))
 
 
 class SnatchAndSnitch(protocol.ReconnectingClientFactory):
-    
+
     factories = 0
     snatches = []
     snitches = []
     connection = None
-    
+
     @classmethod
     def startFactory(cls):
         if not cls.factories:
             cls.connection = sqlite3.connect(settings.database)
             cls.connection.text_factory = str
         cls.factories += 1
-      
-    @classmethod  
+
+    @classmethod
     def stopFactory(cls):
         cls.factories -= 1
         if not cls.factories:
@@ -360,4 +401,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
